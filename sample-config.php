ab048c16341a896186f23242ebcb817baabc591d
abelfacenda <abelfacenda26@gmail.com>
2018-11-21 10:20:25 -0500

    sample-config.php instructions

---
+++ sample-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+Instructions:
+1. Rename this file to config.php
+2. Enter your libcal api and Google settings
+3. Run libcal-harvester.php
+
+Happy harvesting!!!
+ *
+ */
+
+$libcal_client_id = "";
+$libcal_client_secret = "";
